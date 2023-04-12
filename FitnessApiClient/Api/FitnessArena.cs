33494c31a183023a9aa693c7e9d04c5d28728597
Fuzi Zalan <fuzi.zalan@student.ms.sapientia.ro>
2023-04-12 16:53:19 +0300

    Add project files.

---
+++ FitnessApiClient/Api/FitnessArena.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApiClient.Api
+{
+    public class FitnessArena
+    {
+        public int arenaId { get; set; }
+        public string name { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
---
+++ FitnessApiClient/Context/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FitnessApiClient.Api;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApiClient.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<FitnessArena> FitnessArenas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var Server = "myServerName\myInstanceName; Database = myDataBase; User Id = myUsername; Password = myPassword"
+            optionsBuilder.UseSqlServer(Server);
+        }
+    }
+}
